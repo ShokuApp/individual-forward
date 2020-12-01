48b4ca78255ef5b5ad00ed620aac8e7d2edf3526
Lucas Mathieux <lucas.mathieux@epitech.eu>
2020-12-01 11:29:23 +0100

    feat: Rename file with dash case

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -5,8 +5,8 @@ import { ColorSchemeName } from 'react-native';
 
 import NotFoundScreen from '../screens/NotFoundScreen';
 import { RootStackParamList } from '../types';
-import BottomTabNavigator from './BottomTabNavigator';
-import LinkingConfiguration from './LinkingConfiguration';
+import BottomTabNavigator from './bottom-tab-navigator';
+import LinkingConfiguration from './linking-configuration';
 
 // If you are not familiar with React Navigation, we recommend going through the
 // "Fundamentals" guide: https://reactnavigation.org/docs/getting-started
