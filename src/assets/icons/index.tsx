c3bddda1a442b638aba514a23717238c0bb3d869
nathans <nathan.sadre@epitech.eu>
2020-12-01 23:55:35 +0100

    style: Adding icons/ in assets/ with an index

---
+++ src/assets/icons/index.tsx
@@ -0,0 +1,9 @@
+const images = {
+  tabBar: {
+    chefHat: require("./chef-hat.png"),
+    recipeBook: require("./recipe-book.png"),
+    profil: require("./user.png")
+  }
+};
+
+export default images
