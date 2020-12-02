9f1b65511fffdf500a943f3418cda12b1aedf121
nathans <nathan.sadre@epitech.eu>
2020-12-02 15:27:22 +0100

    perf: Navigation Container in the Safe Area Provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
-import { BottomMenu } from "./components/tab-bar/bottom-menu";
+import { BottomMenu } from "./components/tab-bar/bottom-tab-navigator";
 import { registerRootComponent } from "expo";
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <SafeAreaProvider>
+    <SafeAreaProvider>
+      <NavigationContainer>
         <BottomMenu />
-      </SafeAreaProvider>
-    </NavigationContainer>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
 
