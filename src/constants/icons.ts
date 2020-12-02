e074fb0ad07d7bc97b9e31daf876a8ba6dd40610
nathans <nathan.sadre@epitech.eu>
2020-12-02 16:36:20 +0100

    style: Replaced _ to - in icons constant file

--- src/constants/icons.ts
+++ src/constants/icons.ts
@@ -2,15 +2,15 @@
 export const icons: Record<string, any> = {
   tabBar: {
     'mapScreen': {
-      onSelected: require("../../assets/images/chef-hat_selected.png"),
+      onSelected: require("../../assets/images/chef-hat-selected.png"),
       notSelected: require("../../assets/images/chef-hat.png")
     },
     'recipeScreen': {
-      onSelected: require("../../assets/images/recipe-book_selected.png"),
+      onSelected: require("../../assets/images/recipe-book-selected.png"),
       notSelected: require("../../assets/images/recipe-book.png")
     },
     'profileScreen': {
-      onSelected: require("../../assets/images/user_selected.png"),
+      onSelected: require("../../assets/images/user-selected.png"),
       notSelected: require("../../assets/images/user.png")
     }
   }
