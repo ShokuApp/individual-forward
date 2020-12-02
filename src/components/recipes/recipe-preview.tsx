ef0b62ced75844223a03def0b2f04de3703cd6b1
Lucas Arsene <lucas.arsene@epitech.eu>
2020-12-02 10:56:29 +0100

    feat: :construction: adding recipe Preview component (hard coded values)

---
+++ src/components/recipes/recipe-preview.tsx
@@ -0,0 +1,109 @@
+import React from "react";
+import { Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
+import { Icon } from "react-native-elements";
+
+const styles = StyleSheet.create({
+  card: {
+    shadowColor: "rgba(0,0,0, .4)",
+    shadowOffset: { height: 3, width: 3 },
+    shadowOpacity: 0.5,
+    shadowRadius: 2,
+    elevation: 2,
+    height: "26.75%",
+    width: "45.87%",
+    borderRadius: 10,
+  },
+  container: {
+    width: "100%",
+    height: 200,
+    marginBottom: 25,
+    borderRadius: 10,
+    backgroundColor: "#FFFFFF",
+    overflow: "hidden",
+  },
+  recipeTitle: {
+    marginLeft: 8,
+    marginBottom: 10,
+    marginTop: 5,
+    fontSize: 20,
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+
+  textContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  text: {
+    fontWeight: "bold",
+    fontSize: 20,
+  },
+  containerView: {
+    position: "absolute",
+    backgroundColor: "#fff",
+    opacity: 0.75,
+    marginTop: 140,
+    width: "100%",
+    height: 65,
+  },
+  view: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+});
+
+export function RecipePreview() {
+  return (
+    <TouchableOpacity
+      style={styles.card}
+      onPress={() => console.log("Attach display recipe details method")}
+    >
+      <View style={styles.container}>
+        <Image
+          style={styles.image}
+          source={require("../../../assets/images/pasta.jpg")}
+        />
+        <View style={styles.containerView}>
+          <View style={styles.view}>
+            <Text style={styles.recipeTitle}>Recette 1</Text>
+            <Icon
+              style={{ marginRight: 8, marginBottom: 10, marginTop: 5 }}
+              name="heart"
+              type="font-awesome"
+              size={15}
+              color="#000"
+              onPress={() => console.log("Attach favorite method")}
+            />
+          </View>
+          <View style={styles.view}>
+            <View style={styles.view}>
+              <Icon
+                style={{ marginLeft: 8 }}
+                name="clock"
+                type="feather"
+                size={15}
+                color="#000"
+              />
+              <Text style={{ marginLeft: 3, fontSize: 12 }}>15 min</Text>
+            </View>
+            <View style={styles.view}>
+              <Text style={{ marginRight: 3 }}>4,9</Text>
+              <Icon
+                style={{ marginRight: 8 }}
+                name="star-o"
+                type="font-awesome"
+                size={15}
+                color="#000"
+              />
+            </View>
+          </View>
+        </View>
+      </View>
+    </TouchableOpacity>
+  );
+}
