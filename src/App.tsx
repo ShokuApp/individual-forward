a2015ab03307617ca2504e0e69dac753fb20663d
Jeannie <jeannie@localhost.localdomain>
2020-12-02 23:29:52 +0100

    feat: Add list of restaurant preview

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { registerRootComponent } from "expo";
-import { RestaurantPreview } from "./components/restaurants/restaurant-preview";
-
-function App() {
-  return <RestaurantPreview />;
-}
-
-export default registerRootComponent(App);
