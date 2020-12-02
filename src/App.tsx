9d4f1f630ca270ab7b193fcd000609b33015132e
nathans <nathan.sadre@epitech.eu>
2020-12-02 10:14:21 +0100

    style: Removed useless import

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
 import React from "react";
-import {
-  createBottomTabNavigator
-} from "@react-navigation/bottom-tabs";
-import { StyleSheet, Text, View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 import { BottomMenu } from "./components/tab-bar/bottom-menu";
--- src/components/tab-bar/bottom-menu-item.tsx
+++ src/components/tab-bar/bottom-menu-item.tsx
@@ -1,7 +1,5 @@
 import React from "react"
-import { View, SafeAreaView, StyleSheet, Text, Image } from "react-native";
-import { AntDesign } from "@expo/vector-icons";
-import colors from '../../constant/colors';
+import { View, StyleSheet, Image } from "react-native";
 import images from './index';
 
 
