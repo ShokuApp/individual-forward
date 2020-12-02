b07ac9920121addec88ab26ba8f78a0d5ca5ce82
Jeannie <jeannie@localhost.localdomain>
2020-12-02 21:30:37 +0100

    feat: Restaurant preview functional
    Functional with addition of fields: house_number, street, postal_code and city in the datas for the address of the restaurant

--- src/components/restaurants/restaurant-preview.tsx
+++ src/components/restaurants/restaurant-preview.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { FC } from "react";
 import {
   View,
   Text,
@@ -8,6 +8,7 @@ import {
   TouchableOpacity,
 } from "react-native";
 import { Card, ListItem, Button, Icon } from "react-native-elements";
+import { Restaurant } from "../../models";
 
 const styles = StyleSheet.create({
   container: {
@@ -39,13 +40,26 @@ const styles = StyleSheet.create({
     marginLeft: 5,
     marginTop: 5,
   },
+  iconPrice: {
+    marginTop: 2,
+  },
+  iconLocation: {
+    marginRight: 2,
+    marginLeft: 8,
+    marginTop: 5,
+  },
+  iconRestaurant: {
+    marginRight: 7,
+    marginLeft: 5,
+    marginTop: 5,
+  },
   view: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
   },
   viewPrice: {
-    marginRight: 10,
+    marginRight: 8,
     marginLeft: 5,
     marginTop: 10,
     flexDirection: "row",
@@ -53,13 +67,25 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   paragraph: {
-    marginTop: 5,
+    marginTop: 10,
     paddingLeft: 5,
     marginBottom: 0,
     fontSize: 10,
   },
+  adress: {
+    marginTop: 0,
+    paddingLeft: 5,
+    marginBottom: 0,
+    fontSize: 10,
+  },
+  openingTime: {
+    marginTop: 0,
+    paddingLeft: 0,
+    marginBottom: 0,
+    fontSize: 10,
+  },
   textRight: {
-    marginTop: 15,
+    marginTop: 10,
     paddingLeft: 5,
     fontSize: 10,
     flexDirection: "row",
@@ -67,7 +93,7 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   textLeft: {
-    paddingTop: 10,
+    paddingTop: 5,
     paddingRight: 2,
     fontSize: 10,
     flexDirection: "row",
@@ -84,7 +110,7 @@ const styles = StyleSheet.create({
   },
   title: {
     paddingTop: 5,
-    paddingLeft: 5,
+    paddingLeft: 8,
     fontSize: 18,
     flexDirection: "row",
     justifyContent: "space-between",
@@ -92,22 +118,73 @@ const styles = StyleSheet.create({
   },
 });
 
-export function RestaurantPreview() {
+const getOpeningTime = (dataOpeningTime: any) => {
+  var now = new Date();
+  var date = now.getDate();
+  if (dataOpeningTime[date].length === 0) {
+    return <Text style={styles.textRight}>Fermé</Text>;
+  } else if (dataOpeningTime[date].length === 1) {
+    return (
+      <Text style={styles.textRight}>
+        {dataOpeningTime[date][0].from + " - " + dataOpeningTime[date][0].to}
+      </Text>
+    );
+  } else {
+    return (
+      <View style={styles.paragraph}>
+        <Text style={styles.openingTime}>
+          {dataOpeningTime[0][0].from + " - " + dataOpeningTime[0][0].to}
+        </Text>
+        <Text style={styles.openingTime}>
+          {dataOpeningTime[0][1].from + " - " + dataOpeningTime[0][1].to}
+        </Text>
+      </View>
+    );
+  }
+};
+
+const getPriceRange = (price: number) => {
+  let numberIcons;
+  if (price < 15) {
+    numberIcons = 1;
+  } else if (price < 30) {
+    numberIcons = 2;
+  } else {
+    numberIcons = 3;
+  }
+  const icons = [];
+  for (let i = 0; i < numberIcons; i++) {
+    icons.push(
+      <Icon
+        name="euro-symbol"
+        type="material-icons"
+        color="#000000"
+        size={20}
+        style={styles.iconPrice}
+      />
+    );
+  }
+  return <View style={styles.viewPrice}>{icons}</View>;
+};
+
+type Props = {
+  restaurant: Restaurant;
+};
+
+export const RestaurantPreview: FC<Props> = ({ restaurant }: Props) => {
+  let imageRestaurant = { uri: restaurant.image };
   return (
     <TouchableOpacity
       style={styles.container}
       onPress={() => console.log("Attach display restaurant details method")}
     >
       <View style={styles.viewContainer}>
-        <Image
-          style={styles.image}
-          source={require("../../../assets/images/restaurant-preview.jpg")}
-        />
+        <Image style={styles.image} source={imageRestaurant} />
         <View style={styles.viewCard}>
           <View style={styles.view}>
-            <Text style={styles.title}>Restaurant Les 2 Font La Paire</Text>
+            <Text style={styles.title}>{restaurant.name}</Text>
             <View style={styles.view}>
-              <Text style={styles.textMark}>4,6</Text>
+              <Text style={styles.textMark}>{restaurant.average_rate}</Text>
               <Icon
                 name="star"
                 type="font-awesome"
@@ -124,21 +201,26 @@ export function RestaurantPreview() {
                 type="font-awesome"
                 color="#000000"
                 size={25}
-                style={styles.iconText}
+                style={styles.iconLocation}
               />
               <View style={styles.paragraph}>
-                <Text style={styles.paragraph}>11 Grande Rue Nazareth</Text>
-                <Text style={styles.paragraph}>31000 Toulouse</Text>
+                <Text style={styles.adress}>
+                  {restaurant.house_number} {} {restaurant.street}
+                </Text>
+                <Text style={styles.adress}>
+                  {restaurant.postal_code} {}
+                  {restaurant.city}
+                </Text>
               </View>
             </View>
             <View style={styles.view}>
-              <Text style={styles.textLeft}>Burger / Americain</Text>
+              <Text style={styles.textLeft}>{restaurant.description}</Text>
               <Icon
                 name="local-dining"
                 type="material-icon"
                 color="#000000"
                 size={20}
-                style={styles.iconText}
+                style={styles.iconRestaurant}
               />
             </View>
           </View>
@@ -151,25 +233,12 @@ export function RestaurantPreview() {
                 size={20}
                 style={styles.iconText}
               />
-              <Text style={styles.textRight}>Ouvert: 11h-14h</Text>
+              {getOpeningTime(restaurant.opening_time)}
             </View>
-            <View style={styles.viewPrice}>
-              <Icon
-                name="euro-symbol"
-                type="material-icons"
-                color="#000000"
-                size={20}
-              />
-              <Icon
-                name="euro-symbol"
-                type="material-icons"
-                color="#000000"
-                size={20}
-              />
-            </View>
+            {getPriceRange(Number(restaurant.average_price.slice(0, -1)))}
           </View>
         </View>
       </View>
     </TouchableOpacity>
   );
-}
+};
