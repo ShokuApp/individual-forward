6037b91f631abb07d2ba19883801cf2863bb739e
Jeannie <jeannie@localhost.localdomain>
2020-12-03 23:48:02 +0100

    feat: Adding preparation steps component

--- src/components/recipes/recipe-details/recipe-steps.tsx
+++ src/components/recipes/recipe-details/recipe-steps.tsx
@@ -9,21 +9,33 @@ import {
 } from "react-native";
 import { Icon } from "react-native-elements";
 import { Recipe, RecipeStep } from "../../../models";
+import { AntDesign } from "@expo/vector-icons";
 
 const { width, height } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
-  steps: {
-    display: "flex",
-    flexDirection: "column",
-    paddingHorizontal: 5,
+  container: {
+    flexDirection: "row",
   },
-  stepsCounter: {
+  step: {
     paddingHorizontal: 5,
-    height: 25,
-    width: 25,
+    fontSize: 14,
+  },
+  stepsCounterContainer: {
+    width: 20,
+    height: 20,
+    borderRadius: 20 / 2,
     backgroundColor: "white",
-    borderRadius: 25 / 2,
+    alignItems: "center",
+    shadowOffset: { height: 3, width: 0 },
+    shadowOpacity: 0.4,
+    shadowRadius: 3,
+    elevation: 5,
+  },
+  counterText: {
+    paddingTop: 1,
+    color: "#2196F3",
+    fontSize: 14,
   },
 });
 
@@ -33,9 +45,11 @@ type Props = {
 
 export const RecipeSteps: FC<Props> = ({ step }: Props) => {
   return (
-    <View>
-      <Text style={styles.stepsCounter}>{step.name.split(" ").pop()}</Text>
-      <Text style={styles.steps}>{step.description}</Text>
+    <View style={styles.container}>
+      <View style={styles.stepsCounterContainer}>
+        <Text style={styles.counterText}>{step.name.split(" ").pop()}</Text>
+      </View>
+      <Text style={styles.step}>{step.description}</Text>
     </View>
   );
 };
