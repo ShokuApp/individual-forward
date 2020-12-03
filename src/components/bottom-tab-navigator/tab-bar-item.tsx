3f5c38ca5375b36b03181f46e3f07f515a65150a
Guillaume Bonnet <guillaume.bonnet@epitech.eu>
2020-12-03 09:17:42 +0100

    refactor: Prettier format

--- src/components/bottom-tab-navigator/tab-bar-item.tsx
+++ src/components/bottom-tab-navigator/tab-bar-item.tsx
@@ -1,7 +1,6 @@
-import React from "react"
+import React from "react";
 import { View, StyleSheet, Image } from "react-native";
-import { icons } from '../../constants';
-
+import { icons } from "../../constants";
 
 type Props = {
   iconName: string;
@@ -12,20 +11,20 @@ const styles = StyleSheet.create({
   container: {
     height: "100%",
     justifyContent: "center",
-    alignItems: "center"
+    alignItems: "center",
   },
   logo: {
     width: 32,
-    height: 32
-  }
-})
+    height: 32,
+  },
+});
 
 export const TabBarItem = ({ iconName, isCurrent }: Props) => {
-  const icon = isCurrent ? icons.tabBar[iconName].onSelected : icons.tabBar[iconName].notSelected;
+  const icon = isCurrent
+    ? icons.tabBar[iconName].onSelected
+    : icons.tabBar[iconName].notSelected;
   return (
-    <View
-      style={styles.container}
-    >
+    <View style={styles.container}>
       <Image style={styles.logo} source={icon} />
     </View>
   );
