604b0683a9e01f5719a7a6432ef6ac91ce03bc89
Lucas Arsene <lucas.arsene@epitech.eu>
2020-12-03 19:22:07 +0100

    feat: correcting close Icon type

--- src/components/recipes/recipe-details/recipe-details.tsx
+++ src/components/recipes/recipe-details/recipe-details.tsx
@@ -1,25 +1,46 @@
 import React, { FC } from "react";
-import { Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
 import { Icon } from "react-native-elements";
 import { Recipe } from "../../../models";
 import { useNavigation } from "@react-navigation/native";
+import { AntDesign } from "@expo/vector-icons";
 
 type Props = {
   recipe: Recipe;
 };
 
+const height = Dimensions.get("window").height;
+const width = Dimensions.get("window").width;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  icon: {
-    marginTop: 54,
-    marginLeft: 24,
+  closeIcon: {
+    height: 40,
+    width: 40,
+    borderRadius: 40 / 2,
+    backgroundColor: "#2196F3",
+    alignItems: "center",
+    justifyContent: "center",
+    position: "absolute",
+    top: height / 11,
+    left: width / 15,
   },
   image: {
     width: "100%",
     height: "35%",
     position: "absolute",
+  },
+  cardView: {
+    height: -30,
   },
 });
 
@@ -29,12 +50,11 @@ export const RecipeDetails: FC<Props> = ({ recipe }: Props) => {
   return (
     <View style={styles.container}>
       <Image style={styles.image} source={imageSrc} />
-      <View style={styles.icon}>
-        <Icon
+      <View style={styles.closeIcon}>
+        <AntDesign
           name="close"
-          type="material"
-          color="#2196F3"
-          reverse={true}
+          size={25}
+          color="white"
           onPress={() => {
             goBack();
           }}
