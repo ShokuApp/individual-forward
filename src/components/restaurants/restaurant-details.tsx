7acd6c775dbf944143c5471358dbcf684bdf6fc0
mattéo <fauchonmatteo@gmail.com>
2020-12-03 19:16:14 +0100

    WIP feat: add title and close button

--- src/components/restaurants/restaurant-details.tsx
+++ src/components/restaurants/restaurant-details.tsx
@@ -12,6 +12,7 @@ const styles = StyleSheet.create({
   },
   image: {
     height: 200,
+    backgroundColor: "gray",
   },
   closeIcon: {
     height: 40,
@@ -21,6 +22,34 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     position: "absolute",
+    top: height / 11,
+    left: width / 15,
+  },
+  detailsContainer: {
+    top: -30,
+    backgroundColor: "white",
+    height: height,
+    borderTopRightRadius: 30,
+    borderTopLeftRadius: 30,
+    alignItems: "center",
+  },
+  titleContainer: {
+    height: height / 20,
+    width: width,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 20,
+    color: "#2196F3",
+    fontWeight: "bold",
+  },
+  informationsContainer: {
+    backgroundColor: "gray",
+    height: height / 10,
+    width: width / 1.2,
+    justifyContent: "space-between",
+    alignItems: "center",
   },
 });
 
@@ -32,6 +61,14 @@ const RestaurantDetails: FC<Props> = () => {
       <View style={styles.image} />
       <View style={styles.closeIcon}>
         <AntDesign name="close" size={25} color="white" />
+      </View>
+      <View style={styles.detailsContainer}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Restaurant Les 2 Font La Paire</Text>
+        </View>
+        <View style={styles.informationsContainer}>
+          <Text>Adresse</Text>
+        </View>
       </View>
     </View>
   );
