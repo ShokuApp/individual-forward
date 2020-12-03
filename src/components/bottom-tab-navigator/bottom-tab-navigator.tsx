cfb7cf8f38e74334fc06bb7d9f9865ad111900c4
Lucas Arsene <lucas.arsene@epitech.eu>
2020-12-03 15:26:59 +0100

    feat: adding stacks and types for every screens

--- src/components/bottom-tab-navigator/bottom-tab-navigator.tsx
+++ src/components/bottom-tab-navigator/bottom-tab-navigator.tsx
@@ -8,15 +8,48 @@ import ProfileScreen from "../../screens/profile";
 import RestaurantsScreen from "../../screens/restaurants";
 import { StyleSheet, View } from "react-native";
 import RecipesScreen from "../../screens/recipes";
+import RecipeDetailsScreen from "../../screens/recipe-details";
+import { Recipe } from "../../models";
+
 import { createStackNavigator } from "@react-navigation/stack";
 
-const Stack = createStackNavigator();
+export type RecipesStackParamList = {
+  Recipes: undefined;
+  RecipeDetails: { recipe: Recipe };
+};
+const RecipesStack = createStackNavigator<RecipesStackParamList>();
 
-const StackScreen = (name: string, component: React.ComponentType) => {
+export type RestaurantsStackParamList = {
+  Restaurants: undefined;
+};
+const RestaurantsStack = createStackNavigator<RestaurantsStackParamList>();
+
+const Restaurants = () => {
   return () => (
-    <Stack.Navigator>
-      <Stack.Screen name={name} component={component} />
-    </Stack.Navigator>
+    <RestaurantsStack.Navigator>
+      <RestaurantsStack.Screen
+        name={"Restaurants"}
+        component={RestaurantsScreen}
+        options={{ title: "Restaurants" }}
+      />
+    </RestaurantsStack.Navigator>
+  );
+};
+
+const Recipes = () => {
+  return () => (
+    <RecipesStack.Navigator>
+      <RecipesStack.Screen
+        name={"Recipes"}
+        component={RecipesScreen}
+        options={{ title: "Recettes" }}
+      />
+      <RecipesStack.Screen
+        name={"RecipeDetails"}
+        component={RecipeDetailsScreen}
+        options={{ headerShown: true }}
+      />
+    </RecipesStack.Navigator>
   );
 };
 
@@ -34,14 +67,8 @@ export const BottomTabNavigator = () => {
       <Tab.Navigator
         tabBar={(props: BottomTabBarProps) => <TabBar {...props} />}
       >
-        <Tab.Screen
-          name="mapScreen"
-          component={StackScreen("Restaurants", RestaurantsScreen)}
-        />
-        <Tab.Screen
-          name="recipeScreen"
-          component={StackScreen("Recipes", RecipesScreen)}
-        />
+        <Tab.Screen name="mapScreen" component={Restaurants()} />
+        <Tab.Screen name="recipeScreen" component={Recipes()} />
         <Tab.Screen name="profileScreen" component={ProfileScreen} />
       </Tab.Navigator>
     </View>
