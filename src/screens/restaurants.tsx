2cb5d0f6981ff8cdf9debf83a2760730e78f842c
nathans <nathan.sadre@epitech.eu>
2020-12-04 15:05:06 +0100

    style: Self closing component Restaurants in RestaurantsScreen

--- src/screens/restaurants.tsx
+++ src/screens/restaurants.tsx
@@ -2,7 +2,7 @@ import React, { FC } from "react";
 import Restaurants from "../components/restaurants/restaurants";
 
 const RestaurantsScreen: FC = () => {
-  return <Restaurants></Restaurants>;
+  return <Restaurants />;
 };
 
 export default RestaurantsScreen;
