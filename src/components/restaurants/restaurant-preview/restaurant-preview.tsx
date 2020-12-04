3bfa6d5cc95cc6ea6921e486b384199649a3a431
Guillaume Bonnet <guillaume.bonnet@epitech.eu>
2020-12-04 17:10:49 +0100

    refactor: Restaurant preview

--- src/components/restaurants/restaurant-preview/restaurant-preview.tsx
+++ src/components/restaurants/restaurant-preview/restaurant-preview.tsx
@@ -18,31 +18,25 @@ const { width, height } = Dimensions.get("window");
 
 const styles = StyleSheet.create({
   container: {
+    display: "flex",
     height: (30 * height) / 100,
-    minHeight: 250,
-    width: (83.73 * width) / 100,
+    width: (85 * width) / 100,
+    backgroundColor: "#FFFFFF",
     shadowOffset: { height: 3, width: 0 },
     shadowOpacity: 0.4,
     shadowRadius: 3,
-    elevation: 5,
     borderRadius: 10,
     marginBottom: 5,
-  },
-  viewContainer: {
-    height: "100%",
-    width: "100%",
     overflow: "hidden",
-    borderRadius: 10,
-    backgroundColor: "#FFFFFF",
-  },
-  viewCard: {
-    width: "100%",
+    elevation: 5,
   },
   image: {
-    width: "100%",
-    height: "60%",
+    flexGrow: 1,
   },
-  view: {
+  informationContainer: {
+    width: "100%",
+  },
+  rowContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
@@ -51,9 +45,6 @@ const styles = StyleSheet.create({
     paddingTop: 5,
     paddingLeft: 8,
     fontSize: 18,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
   },
 });
 
@@ -69,26 +60,24 @@ export const RestaurantPreview: FC<Props> = ({ restaurant }: Props) => {
       style={styles.container}
       onPress={() => console.log("Attach display restaurant details method")}
     >
-      <View style={styles.viewContainer}>
-        <Image style={styles.image} source={imageRestaurant} />
-        <View style={styles.viewCard}>
-          <View style={styles.view}>
-            <Text style={styles.title}>{restaurant.name}</Text>
-            <Rating rate={restaurant.averageRate} />
-          </View>
-          <View style={styles.view}>
-            <Location
-              streetNumber={restaurant.address.streetNumber}
-              street={restaurant.address.street}
-              postalCode={restaurant.address.postalCode}
-              city={restaurant.address.city}
-            />
-            <RestaurantType description={restaurant.description} />
-          </View>
-          <View style={styles.view}>
-            <Schedule openingTimes={restaurant.openingTime} />
-            <PriceRange price={restaurant.averagePrice} />
-          </View>
+      <Image style={styles.image} source={imageRestaurant} />
+      <View style={styles.informationContainer}>
+        <View style={styles.rowContainer}>
+          <Text style={styles.title}>{restaurant.name}</Text>
+          <Rating rate={restaurant.averageRate} />
+        </View>
+        <View style={styles.rowContainer}>
+          <Location
+            streetNumber={restaurant.address.streetNumber}
+            street={restaurant.address.street}
+            postalCode={restaurant.address.postalCode}
+            city={restaurant.address.city}
+          />
+          <RestaurantType description={restaurant.description} />
+        </View>
+        <View style={styles.rowContainer}>
+          <Schedule openingTimes={restaurant.openingTime} />
+          <PriceRange price={restaurant.averagePrice} />
         </View>
       </View>
     </TouchableOpacity>
