f0df626e53ca2521c3b618a47004545f173ff13c
nathans <nathan.sadre@epitech.eu>
2020-12-05 15:39:12 +0100

    style: removed useless map

--- src/screens/restaurants.tsx
+++ src/screens/restaurants.tsx
@@ -9,7 +9,6 @@ import {
   RestaurantState,
 } from "../blocs";
 import { Restaurants } from "../components/restaurants/restaurants";
-import { Restaurant } from "../models";
 import { RestaurantRepository } from "../repositories";
 
 const RestaurantsScreen: FC = () => {
@@ -21,10 +20,6 @@ const RestaurantsScreen: FC = () => {
       builder={(state: RestaurantState) => {
         if (state instanceof RestaurantErrorState) return <Text>Error</Text>;
         if (state instanceof RestaurantListState) {
-          const restaurantLocationList: Restaurant["location"][] = [];
-          state.restaurants.map((restaurant) => {
-            restaurantLocationList.push(restaurant.location);
-          });
           return <Restaurants restaurantsList={state.restaurants} />;
         }
         return <Text>Loading</Text>;
