ee2e0d5ca2e59957dc4ccc2fd9f022308fc4640a
nathans <nathan.sadre@epitech.eu>
2020-12-07 14:41:28 +0100

    docs: deepcode issue

--- src/components/restaurants/restaurant-map/map-area.tsx
+++ src/components/restaurants/restaurant-map/map-area.tsx
@@ -12,8 +12,8 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   mapStyle: {
-    width: width,
-    height: height,
+    width,
+    height,
   },
   previewList: {
     position: "absolute",
