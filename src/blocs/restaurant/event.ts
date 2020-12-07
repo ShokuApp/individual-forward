a9a7b234ce12ce10991ba863e4a56a2f6afbf9f2
Guillaume Bonnet <28934649+MrSquaare@users.noreply.github.com>
2020-12-07 12:21:23 +0100

    Restore event.ts

--- src/blocs/restaurant/event.ts
+++ src/blocs/restaurant/event.ts
@@ -1,7 +1,5 @@
 export abstract class RestaurantEvent {}
 
-export class RestaurantRefreshEvent extends RestaurantEvent {}
-
 export class RestaurantGetEvent extends RestaurantEvent {
   id: string;
 
