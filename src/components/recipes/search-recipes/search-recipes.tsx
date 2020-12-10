8ca52be15f8af55dbc6a7c005e00e449ae17c8b1
nathans <nathan.sadre@epitech.eu>
2020-12-10 11:33:45 +0100

    refactor: AllergenSelection now containing forme and allergens

--- src/components/recipes/search-recipes/search-recipes.tsx
+++ src/components/recipes/search-recipes/search-recipes.tsx
@@ -1,13 +1,11 @@
-import React, { FC, useState, useEffect } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import React, { FC, useState } from "react";
+import { View, StyleSheet } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { ScrollView } from "react-native-gesture-handler";
 import { SearchBar, Button } from "../../common";
-import { alertForMe } from "../../common/alerts";
-import { MyCheckBox } from "../../common/checkbox";
 import { Pictogram } from "../../../models";
-import { Allergen } from "../../common/allergen";
+import { AllergenSelection } from "../../common/allergen-selection";
 
 const styles = StyleSheet.create({
   container: {
@@ -50,65 +48,24 @@ type Props = {
   profileAllergens: Pictogram[];
 };
 
-const handleAllergensSelected: (
-  allergen: Pictogram,
-  allergensSelected: Pictogram[]
-) => void = (allergen, allergensSelected) => {
-  if (allergensSelected.includes(allergen)) {
-    const index = allergensSelected.indexOf(allergen);
-    allergensSelected.splice(index, 1);
-  } else {
-    allergensSelected.push(allergen);
-  }
-};
-
 export const SearchRecipe: FC<Props> = (props: Props) => {
   const [text, setText] = useState("");
-  const [forMe, setForMe] = useState(true);
   const [allergensSelected, setAllergensSelected] = useState([
     ...props.profileAllergens,
   ]);
 
   const { navigate } = useNavigation();
 
-  useEffect(() => {
-    if (forMe) {
-      setAllergensSelected([...props.profileAllergens]);
-    }
-  }, [forMe]);
-
   return (
     <SafeAreaView style={styles.container} edges={["bottom"]}>
       <ScrollView>
         <SearchBar text={text} setText={setText} />
-        <View style={styles.filterContainer}>
-          <MyCheckBox
-            label={"Pour moi"}
-            check={forMe}
-            onPress={() => {
-              if (forMe) {
-                alertForMe(
-                  setForMe,
-                  "Les recettes proposées pourraient ne plus être sûrs pour vous"
-                );
-              } else {
-                setForMe(!forMe);
-              }
-            }}
-          />
-          <Text style={styles.filterTitle}>Allergènes :</Text>
-          <View style={styles.allergensContainer}>
-            {props.allergens.map((allergen) => (
-              <Allergen
-                key={allergen.id}
-                touchableDisable={forMe}
-                allergen={allergen}
-                handleAllergensSelected={handleAllergensSelected}
-                allergensSelected={allergensSelected}
-              />
-            ))}
-          </View>
-        </View>
+        <AllergenSelection
+          allergensSelected={allergensSelected}
+          setAllergensSelected={setAllergensSelected}
+          allergens={props.allergens}
+          profileAllergens={props.profileAllergens}
+        />
       </ScrollView>
       <View style={styles.searchButton}>
         <Button
--- src/components/restaurants/search-restaurants/search-restaurants.tsx
+++ src/components/restaurants/search-restaurants/search-restaurants.tsx
@@ -1,14 +1,13 @@
-import React, { FC, useState, useEffect } from "react";
+import React, { FC, useState } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import { Pictogram } from "../../../models";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { SearchBar, Button } from "../../common";
 import { MyCheckBox } from "../../common/checkbox";
 import { ScrollView } from "react-native-gesture-handler";
-import { Allergen } from "../../common/allergen";
 import { useNavigation } from "@react-navigation/native";
 import { SearchBy, SEARCH_BY } from "./search-by";
-import { alertForMe } from "../../common/alerts";
+import { AllergenSelection } from "../../common/allergen-selection";
 
 const styles = StyleSheet.create({
   container: {
@@ -51,21 +50,8 @@ type Props = {
   profileAllergens: Pictogram[];
 };
 
-const handleAllergensSelected: (
-  allergen: Pictogram,
-  allergensSelected: Pictogram[]
-) => void = (allergen, allergensSelected) => {
-  if (allergensSelected.includes(allergen)) {
-    const index = allergensSelected.indexOf(allergen);
-    allergensSelected.splice(index, 1);
-  } else {
-    allergensSelected.push(allergen);
-  }
-};
-
 export const SearchRestaurant: FC<Props> = (props: Props) => {
   const [text, setText] = useState("");
-  const [forMe, setForMe] = useState(true);
   const [lowPrice, setLowPrice] = useState(true);
   const [middlePrice, setMiddlePrice] = useState(true);
   const [highPrice, setHighPrice] = useState(true);
@@ -74,31 +60,13 @@ export const SearchRestaurant: FC<Props> = (props: Props) => {
     ...props.profileAllergens,
   ]);
   const { navigate } = useNavigation();
-  useEffect(() => {
-    if (forMe) {
-      setAllergensSelected([...props.profileAllergens]);
-    }
-  }, [forMe]);
+
   return (
     <SafeAreaView style={styles.container} edges={["bottom"]}>
       <ScrollView>
         <SearchBy isSearchBy={isSearchBy} setSearchBy={setSearchBy} />
         <SearchBar text={text} setText={setText} />
         <View style={styles.filterContainer}>
-          <MyCheckBox
-            label={"Pour moi"}
-            check={forMe}
-            onPress={() => {
-              if (forMe) {
-                alertForMe(
-                  setForMe,
-                  "Les restaurants proposés pourraient ne plus être sûrs pour vous"
-                );
-              } else {
-                setForMe(!forMe);
-              }
-            }}
-          />
           <Text style={styles.filterTitle}>Prix :</Text>
           <View style={styles.priceContainer}>
             <MyCheckBox
@@ -117,19 +85,13 @@ export const SearchRestaurant: FC<Props> = (props: Props) => {
               onPress={() => setHighPrice(!highPrice)}
             />
           </View>
-          <Text style={styles.filterTitle}>Allergènes :</Text>
-          <View style={styles.allergensContainer}>
-            {props.allergens.map((allergen) => (
-              <Allergen
-                key={allergen.id}
-                touchableDisable={forMe}
-                allergen={allergen}
-                handleAllergensSelected={handleAllergensSelected}
-                allergensSelected={allergensSelected}
-              />
-            ))}
-          </View>
         </View>
+        <AllergenSelection
+          allergensSelected={allergensSelected}
+          setAllergensSelected={setAllergensSelected}
+          allergens={props.allergens}
+          profileAllergens={props.profileAllergens}
+        />
       </ScrollView>
       <View style={styles.searchButton}>
         <Button
