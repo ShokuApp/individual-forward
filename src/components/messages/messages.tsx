f98066bade59c5b360795bc91195bd0b625d0eaa
mattéo <fauchonmatteo@gmail.com>
2021-01-09 17:27:34 +0100

    feat: WIP messages page

--- src/components/messages/messages.tsx
+++ src/components/messages/messages.tsx
@@ -1,15 +1,18 @@
 import React, { FC } from "react";
 import { Profile } from "../../models";
-import { View, Text } from "react-native";
+import { View, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+  },
+});
 
 type Props = {
   profile: Profile;
 };
 
 export const Messages: FC<Props> = (props: Props) => {
-  return (
-    <View>
-      <Text>Test</Text>
-    </View>
-  );
+  return <View style={styles.container}></View>;
 };
